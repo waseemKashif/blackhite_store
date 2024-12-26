cac61a9a8ff706641b6ec542b5f3f2d1222031d5
waseem kashif <waseemkashif7@gmail.com>
2024-12-26 18:00:14 +0300

    updated the card and pricing section for the product list and products

--- components/shared/header/menu.tsx
+++ components/shared/header/menu.tsx
@@ -1,34 +1,56 @@
 import { Button } from "@/components/ui/button";
 import ModeToggle from "./mode-toggle";
-import { ShoppingCart, UserIcon } from "lucide-react";
+import { EllipsisVertical, ShoppingCart, UserIcon } from "lucide-react";
 import Link from "next/link";
-import { Sheet } from "@/components/ui/sheet";
+import {
+  Sheet,
+  SheetContent,
+  SheetTitle,
+  SheetTrigger,
+} from "@/components/ui/sheet";
 
- const Menu = () => {
-    return (
-      <div className="flex  justify-end gap-3">
-        <nav className=" hidden md:flex w-full max-w-xs gap-1">
-          <ModeToggle />
-          <Button asChild variant="ghost">
-            <Link href="/cart">
-              <ShoppingCart />
-              Cart
-            </Link>
-          </Button>
-          <Button asChild>
-            <Link href="/sign-in">
-              <UserIcon />
-              Sign in
-            </Link>
-          </Button>
-        </nav>
-        <nav className=" md:hidden">
-            {/* <Sheet>
+const Menu = () => {
+  return (
+    <div className="flex  justify-end gap-3">
+      <nav className=" hidden md:flex w-full max-w-xs gap-1">
+        <ModeToggle />
+        <Button asChild variant="ghost">
+          <Link href="/cart">
+            <ShoppingCart />
+            Cart
+          </Link>
+        </Button>
+        <Button asChild>
+          <Link href="/sign-in">
+            <UserIcon />
+            Sign in
+          </Link>
+        </Button>
+      </nav>
+      <nav className=" md:hidden">
+        <Sheet>
+          <SheetTrigger className="  align-middle">
+            <EllipsisVertical />
+          </SheetTrigger>
+          <SheetContent className=" flex flex-col items-start">
+            <SheetTitle>Menu</SheetTitle>
+            <ModeToggle />
+            <Button asChild variant="ghost">
+              <Link href="/cart">
+                <ShoppingCart /> cart{" "}
+              </Link>
+            </Button>
+            <Button asChild>
+              <Link href="/sign-in">
+                <UserIcon />
+                Sign in
+              </Link>
+            </Button>
+          </SheetContent>
+        </Sheet>
+      </nav>
+    </div>
+  );
+};
 
-            </Sheet> */}
-        </nav>
-      </div>
-    );
- }
-  
- export default Menu;
+export default Menu;
---
+++ components/shared/product/product-card.tsx
@@ -0,0 +1,60 @@
+import Link from "next/link";
+import Image from "next/image";
+import { Card, CardContent, CardHeader } from "@/components/ui/card";
+import ProuductPrice from "./product-price";
+import { Button } from "@/components/ui/button";
+const ProductCard = ({ product }: { product: any }) => {
+  return (
+    <Card className=" w-full max-w-sm">
+      <CardHeader className=" p-0  items-center ">
+        <Link href={`/product/${product.slug}`}>
+          <Image
+            src={product.images[0]}
+            alt={product.name}
+            height={300}
+            width={300}
+            priority={true}
+            className=" rounded-md"
+          />
+        </Link>
+      </CardHeader>
+      <CardContent>
+        <div className=" text-xs">{product.brand}</div>
+        <Link href={`/product/${product.slug}`}>
+          <h2
+            className="text-sm font-medium overflow-ellipsis line-clamp-2 h-11"
+            title={product.name}
+          >
+            {product.name}
+          </h2>
+        </Link>
+        <div className="flex flex-col">
+          {product.stock ? (
+            product.price.discountedPrice ? (
+              <div className="flex gap-x-4 ">
+                <ProuductPrice
+                  price={product.price.discountedPrice}
+                  className=" text-destructive font-bold "
+                />
+                <ProuductPrice
+                  price={product.price.regularPrice}
+                  className="discounted-Price relative"
+                />
+              </div>
+            ) : (
+              <ProuductPrice price={product.price.regularPrice} />
+            )
+          ) : (
+            <div className=" flex-between">
+              <Button variant="ghost"> Make Request</Button>
+              <p className="text-destructive font-bold text-sm">Out Of Stock</p>
+            </div>
+          )}
+          <p>{product.rating} Stars</p>
+        </div>
+      </CardContent>
+    </Card>
+  );
+};
+
+export default ProductCard;
