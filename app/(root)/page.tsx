85fb2f760d1530aaa3ba70a6f40d9e6840353274
waseem kashif <waseemkashif7@gmail.com>
2024-12-28 17:58:17 +0300

    added the prisma database, zod schema and types for data

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,12 +1,14 @@
-import sampleData from "@/db/sample-data";
+// import sampleData from "@/db/sample-data";
 import ProductList from "@/components/shared/product/product-list";
+import { getLatestProducts } from "@/lib/actions/product.actions";
 // export const metadata = {
 //   title: "Home",
 // };
-const Homepage = () => {
+const Homepage = async () => {
+  const latestProducts = await getLatestProducts()
   return (
     <>
-      <ProductList data={sampleData.products} title="New Arrival" limit={4} />
+      <ProductList data={latestProducts} title="New Arrival" limit={4} />
     </>
   );
 };
--- components/shared/product/product-price.tsx
+++ components/shared/product/product-price.tsx
@@ -1,16 +1,16 @@
 import { cn } from "@/lib/utils";
+import { formatNumberWithDecimal } from "@/lib/utils";
 
- const ProuductPrice = ({price,className}:{price:number; className?:string}) => {
-    // fixing price to two decimal points
-    const stringValue = price.toFixed(2);
-    // spliting price from decimal point and integar point
-     const [intValue,floatValue]=stringValue.split('.');
-    return ( <p className={cn(' text-2xl',className)}>
-        <span className=" text-xs align-super">QAR</span>
-        {intValue}
-        <span className=" text-xs align-super">.{floatValue}</span>
-
-    </p>);
+ const ProuductPrice = ({price,className}:{price:string; className?:string}) => {
+   const finalPrice = formatNumberWithDecimal(Number(price));
+    const [intValue,floatValue]=finalPrice?.split('.');
+   return (
+     <p className={cn(" text-2xl", className)}>
+       <span className=" text-xs align-super">QAR</span>
+       {intValue}
+       <span className=" text-xs align-super">.{floatValue}</span>
+     </p>
+   );
  }
   
  export default ProuductPrice;
